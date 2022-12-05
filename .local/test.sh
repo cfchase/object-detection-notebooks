3c146accd5d841f9b7d8046e78f2b6d487d62606
Chris Chase <cchase@redhat.com>
2022-12-05 13:23:21 -0500

    initial commit

---
+++ .local/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+printf "\n\n######## dev ########\n"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MY_ROUTE=http://localhost:5000
+MY_IMAGE="${DIR}/twodogs.jpg"
+
+curl ${MY_ROUTE}/status
+
+(echo -n '{"image": "'; base64 "${MY_IMAGE}"; echo '"}') | curl -X POST -H "Content-Type: application/json" -d @- ${MY_ROUTE}/predictions
